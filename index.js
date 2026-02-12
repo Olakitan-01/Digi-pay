7aa7b2ced9c69f5794283f7dac6165753c332019
Olanrewaju Emmanuel <olanrewajuemma2023@gmail.com>
2026-02-12 17:00:19 +0100

    Digi-pay

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db.config');
+const authRoutes = require('./routes/auth.route');
+const User = require('./models/user.model');
+const Account = require('./models/account.model');
+const Transaction = require('./models/transaction.model');
+const userRoutes = require('./routes/user.route');
+const accountRoutes = require('./routes/account.route');
+
+// Connect to MongoDB
+connectDB();    
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json()); 
+
+// Use auth routes
+app.use('/api/auth', authRoutes); 
+// Use user routes
+app.use('/api/users', userRoutes);
+// Use account routes
+app.use('/api/account', accountRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World something huge is cominggggggggggggggggg. It is called Digi!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
